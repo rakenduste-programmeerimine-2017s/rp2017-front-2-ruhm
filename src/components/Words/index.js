d9e534f095e4807e5de9e4d2b327649013af6cfc
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-10-06 13:29:32 +0300

    added class, get words

--- src/components/Words/index.js
+++ src/components/Words/index.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import Api from '../../utils/api'
 
 import Form from './Form'
+import List from './List'
 
-const Words = () => {
-  return (
-    <div id='words'>
-      <h1>Words</h1>
-      <br />
-      <Form />
-    </div>
-  )
+class Words extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  componentDidMount () {
+    console.log('laetud')
+    Api('GET', '/words', {})
+      .then(results => {
+        console.log(results)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  render () {
+    console.log('renderdatud')
+    return (
+      <div id='words'>
+        <h1>Words</h1>
+        <br />
+        <Form />
+        <br />
+        <List />
+      </div>
+    )
+  }
 }
 
 export default Words
