6942259e34dadbf0e976db24582bbf299e7c4f74
randotm <randotm@users.noreply.github.com>
2017-10-18 23:15:07 +0300

    Added state and messages.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,6 +5,7 @@ class Form extends React.Component {
   constructor (props) {
     super(props)
 
+    this.state = {}
     this.saveWord = this.saveWord.bind(this)
   }
 
@@ -18,18 +19,34 @@ class Form extends React.Component {
     })
       .then(results => {
         console.log(results)
+        const { word } = results
+        this.setState({
+          msg: 'Word saved successfully ' + word.name,
+          error: false
+        })
       })
       .catch(error => {
         console.error(error)
+        let errorMessage = error.data.errors.name ? error.data.errors.name.msg : error.data.errors.msg
+        this.setState({
+          error: errorMessage,
+          msg: false
+        })
       })
   }
 
   render () {
+    
+    const { msg, error } = this.state
+    
     return (
+      <div className='word-form'>
+      <p>{msg || error}</p>
       <form onSubmit={this.saveWord}>
         <input id='name' type='text' />
         <input type='submit' value='save' />
       </form>
+      </div>
     )
   }
 }
