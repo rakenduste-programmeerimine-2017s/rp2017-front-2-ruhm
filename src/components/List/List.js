9bc3e88c23e586ac7d9c3e91dfd7c9d142a430eb
randotm <randotm@users.noreply.github.com>
2017-10-18 23:34:32 +0300

    Added list with dummy value to api/words, words are loaded in api/words, not displayed yet though.

---
+++ src/components/List/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = () => {
+  return (
+  	<ul>
+  		<li>Sõna1</li>
+  	</ul>
+  )
+}
+
+export default List
--- src/components/Words/index.js
+++ src/components/Words/index.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import Api from '../../utils/api'
 
 import Form from '../Form/Form'
+import List from '../List/List'
 
-const Words = () => {
-  return (
-    <div id='words'>
-      <h1>Words</h1>
-      <Form />
-    </div>
-  )
+
+class Words extends React.Component {
+	constructor (props) {
+		super(props)
+	}
+
+	componentDidMount () {
+		console.log('laetud')
+		Api('GET', '/words', {})
+		.then( results => {
+			console.log(results)
+		})
+		.catch( error => {
+			console.error(error)
+		})
+	}
+
+	render () {
+  	return (
+	    <div id='words'>
+	      <h1>Words</h1>
+	      <Form />
+	      <br />
+	    <List />
+      </div>
+    )
+  }
+
 }
 
 export default Words
